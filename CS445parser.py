75866fd3072ef6cb1a3ac69ee988ffb93cb527e4
mezamartinez-kimberly <70337928+mezamartinez-kimberly@users.noreply.github.com>
2023-04-26 00:31:54 -0700

    creates defend commands
    also tested w this example alert file using a more basic alert comment

--- CS445parser.py
+++ CS445parser.py
@@ -19,28 +19,76 @@ def locReadFile():
 
 
 #parse first line in contentToParse, find the word sudo and save the rest of line
-def parseFile(contentToParse):
+def parseFirstComment(contentToParse):
     #split the contentToParse into lines
     lines = contentToParse.splitlines()
     #get the first line
     firstLine = lines[0]
     #find the word sudo
-    sudo = re.search('sudo', firstLine)
+    search = re.search('alert', firstLine)
     #get the index of the word sudo
-    sudoIndex = sudo.start()
+    searchIndex = search.start()
     #get the rest of the line
-    comment = firstLine[sudoIndex + 5:]
+    comment = firstLine[searchIndex + 5:]
     #remove the last 4 characters from the comment
     comment = comment[:-4]
     #return the comment 
     return comment
 
+#parse the third line in contentToParse, find the ip first address and save it
+def parseIP(contentToParse):
+    #split the contentToParse into lines
+    lines = contentToParse.splitlines()
+    #get the third line
+    thirdLine = lines[2]
+    #find the ip address
+    search = re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', thirdLine)
+    #get the index of the ip address
+    searchIndex = search.start()
+    #get the ip address
+    ip = thirdLine[searchIndex:]
+    #find the -> and get the index
+    search = re.search('->', ip)
+    #remove the -> and everything after it
+    ip = ip[:search.start()]
+    #return the ip address
+    return ip
+
+#parse the comment for the words XMAS, FIN, or NULL
+def ifStatement(comment, ip):
+    # define sudo iptables -A INPUT string
+    sudoIptables = 'sudo iptables -A INPUT'
+    # define snort rule string for FIn scan
+    snortRuleFin = '-p tcp --tcp-flags ALL FIN'
+    # define snort rule string for NULL scan
+    snortRuleNull = '-p tcp --tcp-flags ALL NULL'
+    # define snort rule string for XMAS scan
+    snortRuleXmas = '-p tcp --tcp-flags ALL XMAS'
+    # define reject string
+    reject = '-j REJECT --reject-with tcp-reject'
+
+    ipAddr = ip
+
+    #if statements for if the comment contains the words XMAS, FIN, or NULL
+    if 'XMAS' in comment:
+        xmasDefend = sudoIptables + ' ' + ipAddr + snortRuleXmas + ' ' + reject
+        return xmasDefend
+    elif 'FIN' in comment:
+        finDefend = sudoIptables + ' ' + ipAddr + snortRuleFin + ' ' + reject
+        return finDefend
+    elif 'NULL' in comment:
+        nullDefend = sudoIptables + ' ' + ipAddr + snortRuleNull + ' ' + reject
+        return nullDefend
+    else:
+        return 'No known scan detected'
 
 
 def main():
-    content = locReadFile()
-    newContent = parseFile(content)
-    print(newContent)
+    content = locReadFile()     #locate and read the alert file
+    comment = parseFirstComment(content)     #parse the first line of the alert file
+    ipAddr = parseIP(content)        #parse ip address call in 3rd line of alert file
+    defend = ifStatement(comment, ipAddr)        #if statement call for if the comment contains the words XMAS, FIN, or NULL
+    print(defend)      #defend is the string that will be used to defend against the attack
 
 if __name__ == "__main__":
     main()
